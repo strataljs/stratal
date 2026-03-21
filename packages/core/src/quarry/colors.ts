de727a9bd8416fe4a00d133ecb83b7d3fd61845f
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2026-03-21 19:41:26 +0000

    feat(core): add NO_COLOR support for ANSI color helpers
    - Introduced a NO_COLOR environment variable to disable ANSI color formatting.
    - Refactored color helper functions to respect the NO_COLOR convention, improving compatibility with environments that do not support color output.

--- packages/core/src/quarry/colors.ts
+++ packages/core/src/quarry/colors.ts
@@ -1,10 +1,9 @@
 /** Minimal ANSI color helpers that respect the `NO_COLOR` convention. */
-const enabled = typeof process !== 'undefined' ? !process.env.NO_COLOR : true
+const isEnabled = () => typeof process !== 'undefined' ? !process.env.NO_COLOR : true
 
 /** Create an ANSI formatter that wraps text with the given open/close SGR codes. */
-const code = (open: number, close: number) => enabled
-  ? (s: string) => `\x1b[${open}m${s}\x1b[${close}m`
-  : (s: string) => s
+const code = (open: number, close: number) => (s: string) =>
+  isEnabled() ? `\x1b[${open}m${s}\x1b[${close}m` : s
 
 export const bold = code(1, 22)
 export const dim = code(2, 22)
@@ -12,6 +11,5 @@ export const cyan = code(36, 39)
 export const green = code(32, 39)
 export const red = code(31, 39)
 export const yellow = code(33, 39)
-export const dimWhite = enabled
-  ? (s: string) => `\x1b[2;37m${s}\x1b[22;39m`
-  : (s: string) => s
+export const dimWhite = (s: string) =>
+  isEnabled() ? `\x1b[2;37m${s}\x1b[22;39m` : s
